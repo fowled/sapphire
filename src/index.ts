4f136afce26a28492eddcfa91c4207ca3b0cfe0e
Max <ma15fo43@gmail.com>
2022-04-15 15:24:03 +0200

    🔥 Switched to `pnpm`, new TS compiler

--- src/index.ts
+++ src/index.ts
@@ -1,146 +1,153 @@
-import * as DiscordRPC from "discord-rpc";
-import * as fs from "fs";
+import DiscordRPC from "discord-rpc";
 import prompt from "prompt";
-import * as path from "path";
-import "colors";
+import chalk from "chalk";
+import path from "path";
+import fs from "fs";
 
 import { log, error } from "./utils/logger";
 
 let applicationID: string;
 
-log("👋 Custom-RPC is now running. Trying to access the config file...".green);
+prompt.message = `${chalk.gray(new Date().toLocaleTimeString())} ${chalk.cyan("[ask]")}`;
+
+log(chalk.green("👋 Custom-RPC is now running. Trying to access the config file..."));
 
 if (fs.existsSync(path.resolve("./config.json"))) {
-    prompt.start();
+	prompt.start();
 
-    prompt.get([
-        {
-            name: "getconfig",
-            description: "✅ Config file found. Do you want to load it? (y/n)".magenta,
-            required: true
-        }
-    ], (err, result) => {
-        if (err) error(err);
+	prompt.get(
+		[
+			{
+				name: "getconfig",
+				description: chalk.magenta("✅ Config file found. Do you want to load it? (y/n)"),
+				required: true,
+			},
+		],
+		(err, result) => {
+			if (err) error(err);
 
-        if (result.getconfig === "y" || result.getconfig === "yes") {
-            gatherInfo();
-        } else {
-            startPrompt();
-        }
-    });
-
+			if (result.getconfig === "y" || result.getconfig === "yes") {
+				gatherInfo();
+			} else {
+				startPrompt();
+			}
+		}
+	);
 } else {
-    log("❗ Config file not found. Starting prompt...".red);
+	log(chalk.red("❗ Config file not found. Starting prompt..."));
 
-    startPrompt();
+	startPrompt();
 }
 
 function gatherInfo() {
-    fs.readFile(path.resolve("./config.json"), "utf8", (err, rawdata) => {
-        if (err) {
-            error(err);
-        }
+	fs.readFile(path.resolve("./config.json"), "utf8", (err, rawdata) => {
+		if (err) {
+			error(err);
+		}
 
-        const data = JSON.parse(rawdata);
+		const data = JSON.parse(rawdata);
 
-        applicationID = data["ID"];
+		applicationID = data["ID"];
 
-        registerRPC(data);
-    });
+		registerRPC(data);
+	});
 }
 
 function startPrompt() {
-    prompt.start();
+	prompt.start();
 
-    prompt.get([
-        {
-            name: "ID",
-            description: "The application ID, found in https://discord.com/developers/applications".yellow,
-            required: true,
-        },
+	prompt.get(
+		[
+			{
+				name: "ID",
+				description: chalk.yellow("The application ID, found in https://discord.com/developers/applications"),
+				required: true,
+			},
 
-        {
-            name: "details",
-            description: "The second line of your RichPresence status".yellow,
-            required: true
-        },
+			{
+				name: "details",
+				description: chalk.yellow("The second line of your RichPresence status"),
+				required: true,
+			},
 
-        {
-            name: "state",
-            description: "The third line of your RichPresence status".yellow,
-            required: true
-        },
+			{
+				name: "state",
+				description: chalk.yellow("The third line of your RichPresence status"),
+				required: true,
+			},
 
-        {
-            name: "largeImageKey",
-            description: "The link to the image you want to display".yellow,
-            required: true
-        },
+			{
+				name: "largeImageKey",
+				description: chalk.yellow("The link to the image you want to display"),
+				required: true,
+			},
 
-        {
-            name: "largeImageText",
-            description: "The description shown above the image".yellow,
-            required: true
-        },
+			{
+				name: "largeImageText",
+				description: chalk.yellow("The description shown above the image"),
+				required: true,
+			},
 
-        {
-            name: "startTimestamp",
-            description: "Enable start timestamp? (y/n)".yellow,
-            required: true
-        },
+			{
+				name: "startTimestamp",
+				description: chalk.yellow("Enable start timestamp? (y/n)"),
+				required: true,
+			},
 
-        {
-            name: "config",
-            description: "Do you want this configuration to be saved in a config.json file? (y/n)".magenta,
-            required: true
-        }
-    ], (err, result) => {
-        if (err) error(err);
+			{
+				name: "config",
+				description: chalk.magenta("Do you want this configuration to be saved in a config.json file? (y/n)"),
+				required: true,
+			},
+		],
+		(err, result) => {
+			if (err) error(err);
 
-        applicationID = result.ID as string;
+			applicationID = result.ID as string;
 
-        if (result.config === "y" || result.config === "yes") {
-            log("🐞 Trying to save your configuration...".blue);
+			if (result.config === "y" || result.config === "yes") {
+				log(chalk.blue("🐞 Trying to save your configuration..."));
 
-            try {
-                const stringifiedData: string = JSON.stringify(result);
+				try {
+					const stringifiedData: string = JSON.stringify(result);
 
-                fs.writeFileSync("config.json", stringifiedData);
-            } catch (err) {
-                error(err as Error);
-            }
-        }
+					fs.writeFileSync("config.json", stringifiedData);
+				} catch (err) {
+					error(err as Error);
+				}
+			}
 
-        registerRPC(result);
-    });
+			registerRPC(result);
+		}
+	);
 }
 
 function registerRPC(info: any) {
-    const RPC = new DiscordRPC.Client({ transport: "ipc" });
-    let startTimestamp: any;
+	const RPC = new DiscordRPC.Client({ transport: "ipc" });
+	let startTimestamp: any;
 
-    if (info.startTimestamp === "y" || info.startTimestamp === "yes") {
-        startTimestamp = new Date();
-    }
+	if (info.startTimestamp === "y" || info.startTimestamp === "yes") {
+		startTimestamp = new Date();
+	}
 
-    function setActivity() {
-        RPC.setActivity({
-            details: info.details,
-            state: info.state,
-            largeImageKey: info.largeImageKey,
-            largeImageText: info.largeImageText,
-            instance: false,
-            startTimestamp
-        });
-    }
+	function setActivity() {
+		RPC.setActivity({
+			details: info.details,
+			state: info.state,
+			largeImageKey: info.largeImageKey,
+			largeImageText: info.largeImageText,
+			instance: false,
+			startTimestamp,
+		});
+	}
 
-    RPC.on("ready", () => {
-        log("👍 Displaying the Rich Presence status...".cyan);
+	RPC.on("ready", () => {
+		log(chalk.cyan("👍 Displaying the Rich Presence status..."));
 
-        setActivity();
-    });
+		setActivity();
+	});
 
-    RPC.login({ clientId: applicationID }).catch(err => {
-        error(err);
-    });
+	RPC.login({ clientId: applicationID }).catch((err) => {
+		error(err);
+	});
 }
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,9 @@
-export function log(message: string) {
-	process.stdout.write(`[${new Date().toLocaleString()}] [LOG]\t${message}\n`);
+import chalk from "chalk";
+
+export function log(message: string): void {
+	process.stdout.write(`${chalk.gray(new Date().toLocaleTimeString())} ${chalk.green("[RPC]")}: ${message}\n`);
 }
 
-export function error(message: string | Error) {
-	process.stderr.write(`\x1b[31m[${new Date().toLocaleString()}] [ERROR]\t${message instanceof Error ? `${message.name}\n\t${message.message}\n\t${message.stack}` : message}\x1b[0m\n`);
+export function error(message: string | Error): void {
+	process.stderr.write(`${chalk.gray(new Date().toLocaleTimeString())} ${chalk.red("[err]")} ${message instanceof Error ? `${message.name}\n\t${message.message}\n\t${message.stack}` : message}\n`);
 }
